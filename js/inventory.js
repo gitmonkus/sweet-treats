fc6a70e8835ddc88034da3b23887384a99605055
Jonathan Aldas <jonaaldas@gmail.com>
2022-01-29 22:45:42 -0500

    finish with inventory

--- js/inventory.js
+++ js/inventory.js
@@ -1,41 +1,83 @@
-const inventory = [
-    {
-        name: 'Ice Cream #1',
-        img : 'https://images.unsplash.com/photo-1579954115563-e72bf1381629?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-        inventory: 10,
-        inStock: true,
-        getsOrderd(num){
-            this.inventory - num
-        }
-    },
-    {
-        name: 'Ice Cream #2',
-        img : 'https://images.unsplash.com/photo-1561845730-208ad5910553?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-        inventory: 12,
-        oinStock: true,
-        getsOrderd(num){
-            this.inventory - num
-        }
-    },
-    {
-        name: 'Ice Cream #3',
-        img : 'https://images.unsplash.com/photo-1485217939211-45a6241d40a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
-        inventory: 20,
-        inStock: true,
-        getsOrderd(num){
-            this.inventory - num
-        }
-    },
-    {
-        name: 'Ice Cream #4',
-        img : 'https://images.unsplash.com/photo-1626552473087-db08c9bb4843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-        inventory: 40,
-        inStock: true,
-        getsOrderd(num){
-            this.inventory - num
-        }
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready(){
+    const removeBtns = document.getElementsByClassName('remove-btn');
+    for (let i = 0; i < removeBtns.length; i++) {
+        let button = removeBtns[i]
+        button.addEventListener('click', removeCartItem)
     }
-]
+
+    let totalUnitsInventory = document.getElementsByClassName('total-units')
+    for (let i = 0; i < totalUnitsInventory.length; i++) {
+        let input = totalUnitsInventory[i]
+        input.addEventListener('change', changeValue)
+    }
+
+    let addToCartBtn = document.getElementsByClassName('order-btn')
+    for (let i = 0; i < addToCartBtn.length; i++) {
+        let button = addToCartBtn[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('checkout-btn')[0].addEventListener('click', purchaseClicked)
+}
+
+
+
+function purchaseClicked(){
+    alert('thank you for your purchase');
+    let elementsInsideCart = document.querySelector('.pending-orders')
+    while(elementsInsideCart.hasChildNodes()){
+        elementsInsideCart.removeChild(elementsInsideCart.firstChild)
+    }
+    updateCartTotal();
+}
+
+function addToCartClicked(event) {
+    let button = event.target
+    let shopItem = button.parentElement;
+    let title = shopItem.getElementsByClassName('shop-item')[0].innerText
+    let price = shopItem.getElementsByClassName('price')[0].innerText
+    let imageSrc = shopItem.getElementsByClassName('img-item')[0].src
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+
+function addItemToCart(title, price, imageSrc) {
+    let cartContainerToAdd = document.createElement('div')
+    // cartRow.classList.add('cart-row')
+    var pendingOrderesCont = document.getElementsByClassName('pending-orders')[0];
+    let checkAllNamesInCart = pendingOrderesCont.getElementsByClassName('cart-item-name');
+    for (let i = 0; i < checkAllNamesInCart.length; i++) {
+            if (checkAllNamesInCart[i].innerText === title) {
+                alert('This item is already added to the cart')
+                return
+            }
+        }
+
+    let HTML = 
+    `
+        <div class="checkout-items">
+        <img class='cart-img'
+        src="${imageSrc}"
+        alt=""
+        />
+        <input type="number" class='total-units' value=3> 
+        <span class="price-from-cart">${price}</span>
+        <p class='cart-item-name'>${title}</p>
+        <button class='remove-btn'>Remove</button>
+        </div>
+    `
+    cartContainerToAdd.innerHTML = HTML
+    pendingOrderesCont.append(cartContainerToAdd);
+    cartContainerToAdd.getElementsByClassName('remove-btn')[0].addEventListener('click', removeCartItem)
+    cartContainerToAdd.getElementsByClassName('total-units')[0].addEventListener('change', changeValue)
+}
 
 
 
@@ -43,97 +85,33 @@ const inventory = [
 
 
 
+function removeCartItem(event) {
+    let buttonClicked = event.target
+    buttonClicked.parentElement.remove()
+    updateCartTotal()
+}
 
 
+function changeValue(event) {
+    let input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //DOM elements 
-// const orderBtn = document.querySelectorAll('.order-btn');
-// const pendingOrdersCont = document.querySelector('.pending-orders');
-// const making = document.querySelector('.making');
-// const pickup = document.querySelector('.pickup');
-
-// let orderAmount = 1;
-
-
-// //inventory left 
-// const inventoryLeft = document.querySelectorAll('.inventory-left')
-// const inventoryLeftArr = Array.from(inventoryLeft);
-
-// orderBtn.forEach(e => {
-//     e.addEventListener('click', ordered)
-// })
-
-// let cloneElement;
-// //variables for the functions 
-// function ordered(){
-//     //we clone the element from inventory and store it into a variable
-//     cloneElement = this.parentElement.cloneNode(true);
-//     //store the btn into a value
-//     let btn = cloneElement.querySelector('.order-btn');
-//     //we change the button text to cancel
-//     btn.innerText = 'Cancel';
-//     //we change the class name;
-//     btn.className = 'cancel-btn';
-//     //we display the container 
-//     making.style.display = 'block'
-//     //append it to the container 
-//     pendingOrdersCont.appendChild(cloneElement);
-// }
-
-
-
-
-// // //store in a varible
-// // let orderBtbn = this;
-// // //we delete the order btn
-// // orderBtbn.innerText = 'Order Again'
-// // //we change the class to order again
-// // orderBtbn.className = 'orderAgain'
-// // //we find all the btn with the new className and store it into a variabl
-// // //this is a node
-// // let orderAgainBtn = document.querySelectorAll('.orderAgain');
-// // // next steps when this button is clicked and it already in the order being preprid section 
-// // // we need add to th value
-
-// // let valueOfInventory;
-// // orderAgainBtn.forEach(e =>{
-// //     e.addEventListener('click', function(){
-// //         let x = cloneElement.querySelectorAll('.inventoryNum');
-// //         valueOfInventory = parseInt(e.parentElement.querySelector('.inventory-left').value) -1;
-// //         let etoNumber;
-// //         x.forEach(e => {
-// //             etoNumber = parseInt(e.value)
-// //         })
-
-// //         let ifOrderedAggain = valueOfInventory + etoNumber;
-// //         console.log(valueOfInventory);
-// //     })
-// // })   
+function updateCartTotal(){
+    let cartItemContainer = document.getElementsByClassName('pending-orders')[0]
+    let checkOutItems = cartItemContainer.getElementsByClassName('checkout-items')
+    let total = 0;
+    for (let i = 0; i < checkOutItems.length; i++) {
+        let cartRow = checkOutItems[i]
+        let priceElement = cartRow.getElementsByClassName('price-from-cart')[0]
+        let quantityElement = cartRow.getElementsByClassName('total-units')[0]
+        let price = parseFloat(priceElement.innerText.replace('$', ''))
+        let quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100;
+    document.getElementsByClassName('total-price-value')[0].innerText = '$' + total;
+}
---
+++ js/object.js
@@ -0,0 +1,38 @@
+// const inventory = [
+//     {
+//         name: 'Ice Cream #1',
+//         img : 'https://images.unsplash.com/photo-1579954115563-e72bf1381629?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+//         inventory: 10,
+//         inStock: true,
+//         getsOrderd(num){
+//             this.inventory - num
+//         }
+//     },
+//     {
+//         name: 'Ice Cream #2',
+//         img : 'https://images.unsplash.com/photo-1561845730-208ad5910553?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+//         inventory: 12,
+//         oinStock: true,
+//         getsOrderd(num){
+//             this.inventory - num
+//         }
+//     },
+//     {
+//         name: 'Ice Cream #3',
+//         img : 'https://images.unsplash.com/photo-1485217939211-45a6241d40a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+//         inventory: 20,
+//         inStock: true,
+//         getsOrderd(num){
+//             this.inventory - num
+//         }
+//     },
+//     {
+//         name: 'Ice Cream #4',
+//         img : 'https://images.unsplash.com/photo-1626552473087-db08c9bb4843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+//         inventory: 40,
+//         inStock: true,
+//         getsOrderd(num){
+//             this.inventory - num
+//         }
+//     }
+// ]
